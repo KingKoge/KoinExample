8283a32e1d875e442815de73ca6a691166a6fcb8
Suttichai Pawapaocapinun <suttichai@livingmobile.me>
2019-04-10 10:04:55 +0700

    create main application

---
+++ app/src/main/java/me/suttichai/develop/koinexample/MainApplication.kt
@@ -0,0 +1,11 @@
+package me.suttichai.develop.koinexample
+
+import android.app.Application
+
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
